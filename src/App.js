1c96996ad257265e279ea4805b9f1784cfbea49f
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-11-25 14:08:01 +0100

    Create Basic Navbar with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home";
 import { Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div>
+      <Navigation />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
