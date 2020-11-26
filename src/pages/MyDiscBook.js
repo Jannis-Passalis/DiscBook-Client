78b54e76222b269f0de3809310afc93d61fed292
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-11-26 14:00:01 +0100

    Create reducer to get info for logged in user with token

--- src/pages/MyDiscBook.js
+++ src/pages/MyDiscBook.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getUserWithStoredToken } from "../store/user/actions";
 
 export default function MyDiscBook() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUserWithStoredToken());
+  }, [dispatch]);
   return (
     <div>
       <h3>MyDiscBook</h3>
