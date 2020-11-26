1f89c5a882f154a5883f34fc3257b4b40393ba4b
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-11-26 17:37:54 +0100

    Connect request with database.server

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Col, Container, Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { signUp } from "../store/user/actions";
+import { selectToken } from "../store/user/selectors";
 
 export default function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [picture, setPicture] = useState("");
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const token = useSelector(selectToken);
+
+  useEffect(() => {
+    if (token !== null) {
+      history.push("/");
+    }
+  }, [token, history]);
+
+  function submitForm(event) {
+    console.log("hi, this is submit sign up console.log");
+    event.preventDefault();
+
+    dispatch(signUp(name, email, password, picture));
+
+    setName("");
+    setEmail("");
+    setPassword("");
+    setPicture("");
+  }
+
   return (
     <Container>
       <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
@@ -9,47 +39,47 @@ export default function SignUp() {
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
           <Form.Control
-            // value={email}
-            // onChange={(event) => setEmail(event.target.value)}
-            // type="email"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            type="name"
             placeholder="Enter name"
-            // required
+            required
           />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Email address</Form.Label>
           <Form.Control
-            // value={password}
-            // onChange={(event) => setPassword(event.target.value)}
-            // type="password"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            type="email"
             placeholder="Enter email"
-            // required
+            required
           />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            // value={password}
-            // onChange={(event) => setPassword(event.target.value)}
-            // type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            type="password"
             placeholder="Enter password"
-            // required
+            required
           />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Profile Picture</Form.Label>
           <Form.Control
-            // value={password}
-            // onChange={(event) => setPassword(event.target.value)}
-            // type="password"
+            value={picture}
+            onChange={(event) => setPicture(event.target.value)}
+            type="text"
             placeholder="Enter a picture. Doesn't have to be a face"
-            // required
+            required
           />
         </Form.Group>
         <Form.Group className="mt-5">
-          <Button variant="outline-dark" type="submit">
-            Log in
+          <Button variant="outline-dark" type="submit" onClick={submitForm}>
+            Sign Up
           </Button>
         </Form.Group>
       </Form>
