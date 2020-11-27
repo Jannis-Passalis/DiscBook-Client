f381fb7d6133ff034f81472e8257a39d9fd386bd
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-11-27 10:46:20 +0100

    Create function for button and state for search bar

--- src/pages/AddCd.js
+++ src/pages/AddCd.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormControl } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 
 export default function AddCd() {
+  const [cdSearch, setCdSearch] = useState("");
+  const dispatch = useDispatch();
+
+  function SubmitNewCdSearch(event) {
+    console.log("hi, this is submit new cd search console.log");
+    event.preventDefault();
+
+    // dispatch(login(cdSearch));
+
+    setCdSearch("");
+  }
+
   return (
     <div>
       <h3 className="App">
         Search for the CD you want to add to your collection.
       </h3>
       <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-dark">Search</Button>
+        <FormControl
+          type="text"
+          placeholder="Search"
+          className="mr-sm-2"
+          value={cdSearch}
+          onChange={(event) => setCdSearch(event.target.value)}
+        />
+        <Button
+          variant="outline-dark"
+          type="submit"
+          onClick={SubmitNewCdSearch}
+        >
+          Search
+        </Button>
       </Form>
     </div>
   );
