fdf015d4e736bfd08f53c62931600abcaf3dc625
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-12-04 10:17:04 +0100

    Add spinner on CDs page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import { Figure, Button, Jumbotron, Container } from "react-bootstrap";
 import FigureCaption from "react-bootstrap/FigureCaption";
 import { selectUser } from "../store/user/selectors";
 import { getUserWithStoredToken } from "../store/user/actions";
+import SpinnerLoading from "../components/SpinnerLoading";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -31,47 +32,49 @@ export default function Home() {
         </Container>
       </Jumbotron>
       <div>
-        {!cds
-          ? "Loading CD's"
-          : cds.map((cd) => {
-              return (
-                <ul key={cd.id}>
-                  <Figure>
-                    <Figure.Image
-                      className="rounded float-left"
-                      width={60}
-                      height={60}
-                      src={cd.albumCover}
-                      rounded
-                      alt="Album Cover"
-                    />
-                    <FigureCaption>
-                      <strong>
-                        {cd.artist ? `${cd.artist} - ` : null}
-                        {cd.album}
-                      </strong>{" "}
-                      {cd.releaseYear ? `(${cd.releaseYear}), ` : null}
-                      Owner: <strong>{cd.list.user.name}</strong>
-                      {user.id !== cd.list.user.id && user.token !== null ? (
-                        cd.forSale ? (
-                          <Button
-                            variant="info"
-                            type="send"
-                            onClick={() =>
-                              dispatch(
-                                sendEmail(user.id, cd.list.user.id, cd.album)
-                              )
-                            }
-                          >
-                            Interested In {cd.list.user.name}'s CD
-                          </Button>
-                        ) : null
-                      ) : null}
-                    </FigureCaption>
-                  </Figure>
-                </ul>
-              );
-            })}
+        {!cds ? (
+          <SpinnerLoading />
+        ) : (
+          cds.map((cd) => {
+            return (
+              <ul key={cd.id}>
+                <Figure>
+                  <Figure.Image
+                    className="rounded float-left"
+                    width={60}
+                    height={60}
+                    src={cd.albumCover}
+                    rounded
+                    alt="Album Cover"
+                  />
+                  <FigureCaption>
+                    <strong>
+                      {cd.artist ? `${cd.artist} - ` : null}
+                      {cd.album}
+                    </strong>{" "}
+                    {cd.releaseYear ? `(${cd.releaseYear}), ` : null}
+                    Owner: <strong>{cd.list.user.name}</strong>
+                    {user.id !== cd.list.user.id && user.token !== null ? (
+                      cd.forSale ? (
+                        <Button
+                          variant="info"
+                          type="send"
+                          onClick={() =>
+                            dispatch(
+                              sendEmail(user.id, cd.list.user.id, cd.album)
+                            )
+                          }
+                        >
+                          Interested In {cd.list.user.name}'s CD
+                        </Button>
+                      ) : null
+                    ) : null}
+                  </FigureCaption>
+                </Figure>
+              </ul>
+            );
+          })
+        )}
       </div>
     </div>
   );
